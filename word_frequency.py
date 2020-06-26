ed2c3d2c53b460897d007d037c8c134ef14ccd52
Rodney Sampson <rodneysampsoniv@gmail.com>
2020-06-26 07:54:24 +0000

    Completes directions in readme  Needs to remove stop words and add asterisk

--- word_frequency.py
+++ word_frequency.py
@@ -1,18 +1,43 @@
 STOP_WORDS = [
-    'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
-    'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
-    'will', 'with'
+    'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has',
+    'he', 'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to',
+    'were', 'will', 'with'
 ]
 
 
+def flatten_lol(lol):
+    flat_list = []
+    for sublist in lol:
+        for item in sublist:
+            flat_list.append(item)
+    return flat_list
+
+
 def print_word_freq(file):
+    d = dict()
+    # string  = ("*")
     """Read in `file` and print out the frequency of words in that file."""
     # Your code will go here. You can write additional functions if you want, and call them inside this function.
     # first open the file
     with open(file) as f:
         lyrics = f.readlines()
-        word_list = [line.split() for line in lyrics]
-        print(word_list)
+        for line in lyrics:
+            #word_list = [line.split() for line in lyrics]
+            #word_list = flatten_lol(word_list)
+            line = line.strip()
+            line = line.lower()
+            words = line.split(" ")
+            for word in words:
+                if word in d:
+                    d[word] = d[word] + 1
+                else:
+                    d[word] = 1
+        for key in list(d.keys()):
+            print(key,":", d[key])
+                #word_list[word] = word.get(word, 0) + 1
+
+    #print('/n.join' (['%s,%s' % (k, v)]))
+
 
 # This is an "incantation." You will not see it very often, and it needs to be here to be able to pass file names as arguments.
 if __name__ == "__main__":
